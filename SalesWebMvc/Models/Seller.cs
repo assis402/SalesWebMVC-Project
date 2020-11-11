c78ed146d1bfc19382c6969548f6d390d11836cb
Matheus De Assis Alcântara <matheus.alcantara@fitbank.com.br>
2020-11-11 10:31:07 -0300

    Simple Create form

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public Seller(int id, string email, string name, DateTime birthDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Email = email;
